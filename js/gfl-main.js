52bf9eafe5eceed41c9cc8b1dd909045a8b0e71f
Tan Nguyen <tan@binaty.org>
2016-01-25 14:19:59 +0700

    Introduction to Event Hook

--- js/gfl-main.js
+++ js/gfl-main.js
@@ -1,3 +1,43 @@
+var GFL = GFL || {};
+
+GFL.events = [];
+
+/**
+ * Subscribe an event. Similar to WP `add_action()`
+ * 
+ * @param  String   name  Event Name
+ * @param  Mixed callback Function Callback
+ * 
+ * @return void
+ */
+GFL.subscribe = function (name, callback) {
+    if (typeof GFL.events[name] == 'undefined')
+        GFL.events[name] = []
+
+    GFL.events[name].push(callback)
+};
+
+/**
+ * Fire an event. Similar to WP `do_action`
+ * 
+ * @param  String name Event Name
+ * @param  Mixed args Function args
+ * 
+ * @return void
+ */
+GFL.fire = function (name, args) {
+    if (typeof GFL.events[name] != 'undefined') {
+        for (var i = 0; i < GFL.events[name].length; ++i) {
+            if (GFL.events[name][i] instanceof Function) {
+                if (typeof args != 'undefined')
+                    GFL.events[name][i](args);
+                else
+                    GFL.events[name][i]();
+            }
+        }
+    }
+};
+
 /**
  * Get Facebook Like Object Class
  * 
@@ -5,31 +45,46 @@
  */
 var GFL_Main = {
 
-	/**
-	 * Initital Method
-	 * 
-	 * @return void
-	 */
-	init: function() {
-		// Listening users like, share, comment event. 
-		// If users did, then send Ajax call to update post meta
-		FB.Event.subscribe('edge.create', GFL_Main.update);
-		FB.Event.subscribe('edge.remove', GFL_Main.update);
-		FB.Event.subscribe('comment.create', GFL_Main.update);
-		FB.Event.subscribe('comment.remove', GFL_Main.update);
-	},
+    /**
+     * Initital Method. Runs on page load.
+     * 
+     * @return void
+     */
+    init: function() {
+        
+        // List of Facebook Events to subscribe. We'll subscribe all of these events by default.
+        var facebookEvents = ['edge.create', 'edge.remove', 'comment.create', 'comment.remove'];
+        
+        for (var i = 0; i < facebookEvents.length; i++)
+        {
+            FB.Event.subscribe(facebookEvents[i], function (url) {
+            	// With each event. We'll allows other extensions to add event listener.
+                GFL.fire(facebookEvents[i], url);
 
-	/**
-	 * Send Ajax to update Likes, Shares and Comments
-	 * 
-	 * @return void
-	 */
-	update: function(url) {
-		jQuery.post( GFL.ajax_url, {
-			action: 'nopriv_update_likes',
-			url: url
-		}, function(response) {
-			console.log(response);
-		});
-	}
+                GFL_Main.update(url);
+            });
+        }
+    },
+
+    /**
+     * Send Ajax to update Likes, Shares and Comments
+     * 
+     * @return void
+     */
+    update: function(url) {
+
+    	// Fire event before update
+        GFL.fire('update.before');
+
+        jQuery.post( GFL.ajax_url, {
+            action: 'nopriv_update_likes',
+            url: url
+        }, function(response) {
+        	// Fire response event. User can use response argument in their function.
+            GFL.fire('update.response', response);
+        });
+
+        // Fire event after update
+        GFL.fire('update.after');
+    }
 };
